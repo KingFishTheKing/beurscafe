43e436beb3165ac858d1a0a57047c775d5319b35
K.Y <K.Y.@not_valid.co.uk>
2019-07-10 15:53:28 +0200

    Working build

---
+++ build/precache-manifest.350ac2fd2c73f3ea73ee7ae2f9fbf99a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aab379d10a1eedbe65e6c67376213bed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1cf4d517e54282cd8268",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "46e9e43247e48824c55c",
+    "url": "/static/css/main.8bca2533.chunk.css"
+  },
+  {
+    "revision": "1cf4d517e54282cd8268",
+    "url": "/static/js/2.73f7deda.chunk.js"
+  },
+  {
+    "revision": "46e9e43247e48824c55c",
+    "url": "/static/js/main.18ff7561.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
